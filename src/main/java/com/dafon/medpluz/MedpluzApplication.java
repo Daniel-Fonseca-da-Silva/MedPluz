eeca1f3e3a4b5177a4e7743d1dfb3f2b058bfac2
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2023-09-07 22:38:19 -0300

    feature: Created the base of project with sring initializr

---
+++ src/main/java/com/dafon/medpluz/MedpluzApplication.java
@@ -0,0 +1,13 @@
+package com.dafon.medpluz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedpluzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedpluzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dafon/medpluz/MedpluzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dafon.medpluz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedpluzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
