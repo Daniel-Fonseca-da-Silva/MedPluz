ea387a0b16b06b52dc064d57358ec402c0535a67
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2023-09-11 14:21:13 -0300

    feature: Pagination and ordination

--- src/main/java/com/dafon/medpluz/controller/MedicoController.java
+++ src/main/java/com/dafon/medpluz/controller/MedicoController.java
@@ -10,6 +10,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -29,7 +30,7 @@ public class MedicoController {
     }
 
     @GetMapping()
-    public Page<DadosListagemMedicoDto> listar(Pageable paginacao) {
+    public Page<DadosListagemMedicoDto> listar(@PageableDefault(size = 2, sort = {"nome"}) Pageable paginacao) {
         return repository.findAll(paginacao).map(DadosListagemMedicoDto::new);
     }
 
