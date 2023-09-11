396746bf63095269ff717186b6731defac0ec39f
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2023-09-10 23:34:43 -0300

    feature: Created the controller of medico

--- src/main/java/com/dafon/medpluz/controller/MedicoController.java
+++ src/main/java/com/dafon/medpluz/controller/MedicoController.java
@@ -1,6 +1,12 @@
 package com.dafon.medpluz.controller;
 
 import com.dafon.medpluz.controller.dto.DadosCadastroMedicoDto;
+import com.dafon.medpluz.model.Endereco;
+import com.dafon.medpluz.model.Medico;
+import com.dafon.medpluz.repository.MedicoRepository;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,10 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/medicos")
 public class MedicoController {
 
-    @PostMapping()
-        public void cadastrar(@RequestBody DadosCadastroMedicoDto dados) {
-        System.out.println(dados);
+    @Autowired
+    private MedicoRepository repository;
 
+    @PostMapping()
+    @Transactional()
+        public void cadastrar(@RequestBody @Valid DadosCadastroMedicoDto dados) {
+            repository.save(new Medico(dados));
     }
 
 }
