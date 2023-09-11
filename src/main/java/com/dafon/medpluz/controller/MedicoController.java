478f1defcabdb7a449ace2e1e72458302710d773
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2023-09-11 11:06:38 -0300

    feature: Created the get method

--- src/main/java/com/dafon/medpluz/controller/MedicoController.java
+++ src/main/java/com/dafon/medpluz/controller/MedicoController.java
@@ -1,16 +1,16 @@
 package com.dafon.medpluz.controller;
 
 import com.dafon.medpluz.controller.dto.DadosCadastroMedicoDto;
+import com.dafon.medpluz.controller.dto.DadosListagemMedicoDto;
 import com.dafon.medpluz.model.Endereco;
 import com.dafon.medpluz.model.Medico;
 import com.dafon.medpluz.repository.MedicoRepository;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/medicos")
@@ -22,7 +22,13 @@ public class MedicoController {
     @PostMapping()
     @Transactional()
         public void cadastrar(@RequestBody @Valid DadosCadastroMedicoDto dados) {
-            repository.save(new Medico(dados));
+
+        repository.save(new Medico(dados));
+    }
+
+    @GetMapping()
+    public List<DadosListagemMedicoDto> listar() {
+        return repository.findAll().stream().map(DadosListagemMedicoDto::new).toList();
     }
 
 }
