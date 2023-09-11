9b1c6fccc0c24c295f5af7a3c6fac671ba99c1b2
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2023-09-10 23:35:56 -0300

    feature: Created the model of endereco

---
+++ src/main/java/com/dafon/medpluz/model/Endereco.java
@@ -0,0 +1,31 @@
+package com.dafon.medpluz.model;
+
+import com.dafon.medpluz.controller.dto.DadosEnderecoDto;
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Endereco {
+    private String logradouro;
+    private String bairro;
+    private String cep;
+    private String numero;
+    private String complemento;
+    private String cidade;
+    private String uf;
+
+    public Endereco(DadosEnderecoDto dados) {
+        this.logradouro = dados.logradouro();
+        this.bairro = dados.bairro();
+        this.cep = dados.cep();
+        this.uf = dados.uf();
+        this.cidade = dados.cidade();
+        this.numero = dados.numero();
+        this.complemento = dados.complemento();
+    }
+}
