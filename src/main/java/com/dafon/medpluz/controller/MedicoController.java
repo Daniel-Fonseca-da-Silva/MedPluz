47b365da8d8ab401adb2e2e691e8d7406092a243
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2023-09-11 23:34:44 -0300

    feature: Created the delete physical of medicos

--- src/main/java/com/dafon/medpluz/controller/MedicoController.java
+++ src/main/java/com/dafon/medpluz/controller/MedicoController.java
@@ -3,18 +3,15 @@ package com.dafon.medpluz.controller;
 import com.dafon.medpluz.controller.dto.DadosAtualizaMedicoDto;
 import com.dafon.medpluz.controller.dto.DadosCadastroMedicoDto;
 import com.dafon.medpluz.controller.dto.DadosListagemMedicoDto;
-import com.dafon.medpluz.model.Endereco;
 import com.dafon.medpluz.model.Medico;
 import com.dafon.medpluz.repository.MedicoRepository;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.*;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/medicos")
@@ -40,6 +37,12 @@ public class MedicoController {
     public void atualizar(@RequestBody @Valid DadosAtualizaMedicoDto dados) {
         var medico = repository.getReferenceById(dados.id());
         medico.atualizarInformacoes(dados);
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional()
+    public void excluir(@PathVariable Long id) {
+        repository.deleteById(id);
     }
 
 }
