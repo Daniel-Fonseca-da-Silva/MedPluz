ed59068c6436034632b8ab7fe9853554a8ebeb69
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2023-09-14 23:41:38 -0300

    feature: Created the global exception when the values are null

--- src/main/java/com/dafon/medpluz/exceptions/TratadorDeErros.java
+++ src/main/java/com/dafon/medpluz/exceptions/TratadorDeErros.java
@@ -2,6 +2,8 @@ package com.dafon.medpluz.exceptions;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 @RestControllerAdvice
@@ -11,4 +13,18 @@ public class TratadorDeErros {
     public ResponseEntity trataErro404() {
         return ResponseEntity.notFound().build();
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity trataErro400(MethodArgumentNotValidException ex) {
+        var erros = ex.getFieldErrors();
+
+        return ResponseEntity.badRequest().body(erros.stream().map(DadosErrosValidacaoDto::new).toList());
+    }
+
+    private record DadosErrosValidacaoDto(String campo, String mensagem) {
+        public DadosErrosValidacaoDto(FieldError erro) {
+            this(erro.getField(), erro.getDefaultMessage());
+        }
+    }
+
 }
