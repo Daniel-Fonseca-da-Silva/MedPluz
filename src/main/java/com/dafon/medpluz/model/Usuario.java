d9d07bbf1c1cee3fadad158e1dce16b5b90ba2dc
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2023-09-15 02:34:13 -0300

    feature: Created the model usuarios with migration inside project

---
+++ src/main/java/com/dafon/medpluz/model/Usuario.java
@@ -0,0 +1,18 @@
+package com.dafon.medpluz.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Table(name = "usuarios")
+@Entity(name = "Usuario")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String login;
+    private String senha;
+}
